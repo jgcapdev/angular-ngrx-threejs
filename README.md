b0e6eb3b6bdf97a7f8d9fe3c69c7be0221f3f2f6
jgcapdev <jorge.gomez-carrillo-external@capgemini.com>
2022-06-23 09:50:35 +0200

    feat: adding Soba and more doc

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
-# Angular - Three.js - NGRX
+![image](docs/images/cover.JPG)
+
+<div align="center">
+    <b>Basic scaffolding for Angular Three projects with NgRx</b>
+</div>
+
+## Table of contents 👇
+
+- [✨ How to install](#-how-to-install)
+- []
+- - [🚀 Netlify](#-netlify)
+
+## ✨ How to install
+
+Simply run `git clone` and then `npm install`.
+
+## 🚀 Netlify
+
+The project is deployed on **Netlify**.
 
 - Install Angular Three with `npm install --save-dev @angular-three/schematics ` and `npx nx generate @angular-three/schematics:init`
 - Install NGRX with `nnpm install @ngrx/store --save`.
 - If there is any error during the deployment process in Netlify use an **environment variable** called `NPM_FLAGS` with `--legacy-peer-deps`.
+
+If you need to install another Angular Three dependency (for example Soba) add it with `npm i @angular-three/soba --legacy-peer-deps`.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,38 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+
+// COUNTER
 import { CounterComponent } from './counter/counter/counter.component';
 import { CounterButtonsComponent } from './counter/counter-buttons/counter-buttons.component';
 import { CounterOutputComponent } from './counter/counter-output/counter-output.component';
+
+// NGRX
 import { StoreModule } from '@ngrx/store';
 import { counterReducer } from './counter/state/counter.reducer';
+
+// ANGULAR THREE
+import { NgtCanvasModule } from '@angular-three/core';
+import { NgtMeshModule } from '@angular-three/core/meshes';
+import {
+  NgtBoxGeometryModule,
+  NgtConeGeometryModule,
+  NgtPlaneGeometryModule,
+  NgtSphereGeometryModule,
+} from '@angular-three/core/geometries';
+import {
+  NgtMeshBasicMaterialModule,
+  NgtMeshStandardMaterialModule,
+} from '@angular-three/core/materials';
+import {
+  NgtAmbientLightModule,
+  NgtSpotLightModule,
+  NgtPointLightModule,
+  NgtDirectionalLightModule,
+} from '@angular-three/core/lights';
+import { NgtSobaOrbitControlsModule } from '@angular-three/soba/controls';
 
 @NgModule({
   declarations: [
@@ -20,6 +46,19 @@ import { counterReducer } from './counter/state/counter.reducer';
     BrowserModule,
     AppRoutingModule,
     StoreModule.forRoot({ counter: counterReducer }),
+    NgtCanvasModule,
+    NgtMeshModule,
+    NgtBoxGeometryModule,
+    NgtConeGeometryModule,
+    NgtPlaneGeometryModule,
+    NgtSphereGeometryModule,
+    NgtMeshBasicMaterialModule,
+    NgtMeshStandardMaterialModule,
+    NgtAmbientLightModule,
+    NgtSpotLightModule,
+    NgtPointLightModule,
+    NgtDirectionalLightModule,
+    NgtSobaOrbitControlsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
